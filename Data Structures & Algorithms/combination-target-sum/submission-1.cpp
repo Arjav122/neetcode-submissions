42abea6ed6c91af3fb4763c8128409161eb54f36
Arjav122 <aarjav122@gmail.com>
2026-07-19 17:42:42 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void f(vector<int>& nums, int target, int i, vector<vector<int>> &ans, vector<int> &temp){
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+
+        if(target < 0){
+            return;
+        }
+
+        for(int j=i; j<nums.size(); j++){
+            temp.push_back(nums[j]);
+            f(nums, target-nums[j], j, ans, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        f(nums, target, 0, ans, temp);
+        return ans;
+    }
+};
