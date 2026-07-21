0613d68c69587cf1980d45d50794171ca2f796ae
Arjav122 <aarjav122@gmail.com>
2026-07-21 21:49:35 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits){
+        int n = digits.size();
+        if(digits[n-1]<9){
+            digits[n-1] = digits[n-1]+1;
+            return digits;
+        }
+        int i=n-1;
+        while(i>=0 and digits[i]==9){
+            digits[i]=0;
+            i--;
+        }
+        if(i<0){
+            digits.insert(digits.begin(), 1);
+            return digits;
+        }else{
+            digits[i] = digits[i]+1;
+            return digits;
+        }
+    }
+};
