6fe1e32d0b473fda999b8840eb722e7c08310c89
Arjav122 <aarjav122@gmail.com>
2026-07-30 16:21:23 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        if (intervals.empty()) return 0;
+        sort(intervals.begin(), intervals.end(), [](Interval &a, Interval &b){
+            return a.start < b.start;
+        });
+
+        priority_queue<int, vector<int>, greater<int>> minheap;
+        minheap.push(intervals[0].end);
+        for(int i=1; i<intervals.size(); i++){
+            if(intervals[i].start >= minheap.top()) minheap.pop();
+            minheap.push(intervals[i].end);
+        }
+        return minheap.size();
+    }
+};
